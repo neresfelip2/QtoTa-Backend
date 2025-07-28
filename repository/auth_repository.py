260405af849004e34b1fbf67837e7bcea147ba38
neresfelip <neresfelip@outlook.com>
2025-07-28 12:33:07 -0300

    Refactor token creation in auth repository: update create_token function to accept User object and include user details in token payload

--- repository/auth_repository.py
+++ repository/auth_repository.py
@@ -1,11 +1,11 @@
 from datetime import datetime, timedelta, timezone
-from main import bcrypt_context, SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
+from main import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
 from jose import jwt
 from database.models import User
 
-def create_token(id_user, duration=timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)):
+def create_token(user: User, duration=timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)):
     expiration_date = datetime.now(timezone.utc) + duration
-    dict_info = { "sub": str(id_user), "exp": expiration_date }
+    dict_info = { "sub": str(user.id), "name": user.name, "email": user.email, "exp": expiration_date }
     token = jwt.encode(dict_info, SECRET_KEY, ALGORITHM)
     return token
 
--- routes/auth_routes.py
+++ routes/auth_routes.py
@@ -19,8 +19,8 @@ async def register(user_scheme: UserScheme, session = Depends(get_session)):
         new_user = User(user_scheme.name, user_scheme.email, encrypted_password)
         session.add(new_user)
         session.commit()
-        access_token = create_token(new_user.id)
-        refresh_token = create_token(new_user.id, duration=timedelta(days=7))
+        access_token = create_token(new_user)
+        refresh_token = create_token(new_user, duration=timedelta(days=7))
         return {
             "access_token": access_token,
             "refresh_token": refresh_token,
@@ -35,8 +35,8 @@ async def login(login_scheme: LoginSchema, session = Depends(get_session)):
     elif not bcrypt_context.verify(login_scheme.password, user.password):
         raise HTTPException(status_code=400, detail="Senha inválida")
     else:
-        access_token = create_token(user.id)
-        refresh_token = create_token(user.id, duration=timedelta(days=7))
+        access_token = create_token(user)
+        refresh_token = create_token(user, duration=timedelta(days=7))
         return {
             "access_token": access_token,
             "refresh_token": refresh_token,
@@ -51,7 +51,7 @@ async def login(form_data: OAuth2PasswordRequestForm = Depends(), session = Depe
     elif not bcrypt_context.verify(form_data.password, user.password):
         raise HTTPException(status_code=400, detail="Senha inválida")
     else:
-        access_token = create_token(user.id)
+        access_token = create_token(user)
         return {
             "access_token": access_token,
             "token_type" : "Bearer"
@@ -59,7 +59,7 @@ async def login(form_data: OAuth2PasswordRequestForm = Depends(), session = Depe
 
 @auth_router.get("/refresh")
 async def user_refresh_token(user: User = Depends(verify_token)):
-    access_token = create_token(user.id)
+    access_token = create_token(user)
     return {
             "access_token": access_token,
             "token_type" : "Bearer"
